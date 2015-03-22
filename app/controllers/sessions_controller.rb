7d82c82c03825ca36b6bab10aaf901dffbccc8eb
olinares <olinares9833@gmail.com>
2015-03-21 23:40:41 -0700

    added some readme stuff, sudo code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,23 @@ class SessionsController < ApplicationController
   layout false
 
   def new
+    # @auth = Auth.new
   end
 
   def create
     # Storing token ID in session
-    token = Token.create(
+    @auth = request.env['omniauth.auth'].to_hash
+
+    # find or create user by auth[provider ]and auth[uid]
+    # update user details
+    # on the user, create a token like you're doing below
+    byebug
+    token = Tokens.create(
+    user_id: user.id,
     access_token: @auth['token'],
     refresh_token: @auth['refresh_token'],
     expires_at: Time.at(@auth['expires_at']).to_datetime)
-    session[:token_id] = token.id
+    session[:user_id] = user.id
     redirect_to root_path
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,15 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
-    scope: ['drive',
-    'https://www.googleapis.com/auth/drive'],
-    access_type: 'offline'}
+  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'],
+    {
+      # :name => "google",
+      # :scope => "email, profile, drive, https://www.googleapis.com/auth/drive",
+      # :prompt => "select_account",
+      # :image_aspect_ratio => "square",
+      # :image_size => 50,
+      # :access_type => "offline"
+
+      scope: ['drive',
+      'https://www.googleapis.com/auth/userinfo.profile'],
+      access_type: 'offline'
+    }
 end
