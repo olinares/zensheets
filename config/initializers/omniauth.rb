5efae9ac71bf7511ba0fafacb18d9d82484ce7c7
olinares <olinares9833@gmail.com>
2015-04-06 15:46:48 -0700

    omniauth env remove plus scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'],
     {
       scope: [
-        'https://www.googleapis.com/auth/plus.login',
         'https://spreadsheets.google.com/feeds',
         'https://www.googleapis.com/auth/drive',
         'https://docs.google.com/feeds',
